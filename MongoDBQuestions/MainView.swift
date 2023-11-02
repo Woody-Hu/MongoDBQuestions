fe9c2b8d05cc9c6af8b21360e080fa51066b2f1a
Stephen Hu <stephenhu@seismic.com>
2023-11-02 15:06:25 +0800

    add project codes

--- MongoDBQuestions/MainView.swift
+++ MongoDBQuestions/MainView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello")
+                .font(.title)
+                .padding(20)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            VStack {
+                Button(action: {
+                    print("Start button hit")
+                })
+                {
+                   Text("Start")
+                }
+                .padding(20)
+                Button(action: {
+                    print("History button hit")
+                })
+                {
+                    Text("History")
+                }
+                .padding(20)
+                Button(action: {
+                    print("about button hit")
+                })
+                {
+                    Text("About")
+                }
+                .padding(20)
+                Button(action: {
+                    print("exit button hit")
+                    exit(0) // 退出应用程序
+                }) 
+                {
+                    Text("Exit")
+                }
+                .padding(20)
+            }
+        }
     }
 }
 
