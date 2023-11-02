585411c6522f37eb78ef0a333343c86720c211ec
Stephen Hu <stephenhu@seismic.com>
2023-11-02 15:02:39 +0800

    add project base codes

---
+++ MongoDBQuestions/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  MongoDBQuestions
+//
+//  Created by Stephen Hu on 2023/11/2.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
