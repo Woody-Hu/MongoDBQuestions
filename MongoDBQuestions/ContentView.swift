3cc6ba4063b93c0c1caba8e2e4003fe2d938d751
Stephen Hu <stephenhu@seismic.com>
2023-11-02 14:44:57 +0800

    Initial Commit

---
+++ MongoDBQuestions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MongoDBQuestions
+//
+//  Created by Stephen Hu on 2023/11/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MongoDBQuestions/MongoDBQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MongoDBQuestionsApp.swift
+//  MongoDBQuestions
+//
+//  Created by Stephen Hu on 2023/11/2.
+//
+
+import SwiftUI
+
+@main
+struct MongoDBQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
