df347466f2353b2c18fe77a6abd8965c641d945a
Stephen Hu <stephenhu@seismic.com>
2023-11-09 16:08:58 +0800

    modify request record

--- MongoDBQuestions/RequestRecord.swift
+++ MongoDBQuestions/RequestRecord.swift
@@ -11,9 +11,19 @@ struct RequestRecord:Identifiable
 {
     let id:UUID
     let requestId:String
+    let finishDate:Date
+    let score:Int
+    let questionIds: [String]
+    let rightQuestionIds : [String]
+    let failQuestionIds: [String]
     
-    init(id: UUID, requestId: String) {
+    init(id: UUID, requestId: String, finishDate:Date) {
         self.id = id
         self.requestId = requestId
+        self.finishDate = finishDate
+        self.score = 0
+        self.questionIds = []
+        self.rightQuestionIds = []
+        self.failQuestionIds = []
     }
 }
