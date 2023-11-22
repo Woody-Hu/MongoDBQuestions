51491e3007baf4877c112a259b221123cfd2d748
Stephen Hu <stephenhu@seismic.com>
2023-11-22 14:28:40 +0800

    add some bisic vieiw logic

---
+++ MongoDBQuestions/QuestionSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionSelectionView.swift
+//  MongoDBQuestions
+//
+//  Created by Stephen Hu on 2023/11/22.
+//
+
+import SwiftUI
+
+struct QuestionSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionSelectionView()
+}
---
+++ MongoDBQuestions/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  MongoDBQuestions
+//
+//  Created by Stephen Hu on 2023/11/22.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionView()
+}
