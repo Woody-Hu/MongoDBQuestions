6fc5682b01d6a30f32456264bc7bfce1cb33cfaf
Stephen Hu <stephenhu@seismic.com>
2023-11-24 18:05:04 +0800

    change the layout about list view

--- MongoDBQuestions/QuestionListView.swift
+++ MongoDBQuestions/QuestionListView.swift
@@ -8,27 +8,24 @@
 import SwiftUI
 
 struct QuestionListView: View {
-    var categories: [String] = []
+    var categories: [String] = ["test1", "test2", "test3"]
     @State var questionsCount = 10.0
-    @State var selectCategory: String = ""
     var body: some View {
-        
         NavigationView{
             VStack
             {
-                Slider(value: $questionsCount, in: 10...30, step: 1){
-                    Text("Number of questions: \(questionsCount)")}.padding()
-                Text("\(Int(questionsCount))")
-                Picker(selection: $selectCategory, label: Text("Select a category")) {
-                    ForEach(categories, id: \.self) {
-                        Text($0)
-                    }
-                }.padding()
+                ForEach(categories, id: \.self){ category in
+                    NavigationLink(destination: QuestionDetailView()){
+                        Text(category)
+                    }.padding()
+                }
                 
                 Spacer()
                 
-                NavigationLink(destination: QuestionDetailView()) {
-                    Text("Start") }.padding()
+                Text("question size")
+                Slider(value: $questionsCount, in: 10...30, step: 1){
+                    Text("Number of questions: \(questionsCount)")}.padding()
+                Text("\(Int(questionsCount))")
             }
         }
     }
