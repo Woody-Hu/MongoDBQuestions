4194303ca570b5262f82301e8b04007e3bdd1814
Stephen Hu <stephenhu@seismic.com>
2023-11-28 15:50:19 +0800

    add category domain

---
+++ MongoDBQuestions/QuestionCategoryRow.swift
@@ -0,0 +1,22 @@
+//
+//  QuestionCategoryRow.swift
+//  MongoDBQuestions
+//
+//  Created by Stephen Hu on 2023/11/28.
+//
+
+import SwiftUI
+
+struct QuestionCategoryRow: View {
+    var body: some View {
+        HStack(content: {
+            /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+        })
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuestionCategoryRow()
+}
