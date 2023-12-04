7de216b23bc1d9fc61301a3d899b04d2f384580f
Stephen Hu <stephenhu@seismic.com>
2023-12-04 15:35:28 +0800

    add button component

---
+++ MongoDBQuestions/ConfirmButtonComponent.swift
@@ -0,0 +1,18 @@
+//
+//  ConfirmButtonComponent.swift
+//  MongoDBQuestions
+//
+//  Created by Stephen Hu on 2023/12/4.
+//
+
+import SwiftUI
+
+struct ConfirmButtonComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConfirmButtonComponent()
+}
