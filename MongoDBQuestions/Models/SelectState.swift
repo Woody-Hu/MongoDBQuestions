3f65eed398995182ffd90d74512a07847f9a2f2a
Stephen Hu <stephenhu@seismic.com>
2023-12-12 14:58:48 +0800

    add select option row component

--- MongoDBQuestions/Models/SelectState.swift
+++ MongoDBQuestions/Models/SelectState.swift
@@ -6,10 +6,25 @@
 //
 
 import Foundation
+import SwiftUI
 enum SelectState:String
 {
     case selectRight
     case selectFail
     case unselectFail
     case normal
+    
+    var color : Color
+    {
+        switch self{
+            case .selectRight:
+                return Color.green
+            case .selectFail:
+                return Color.red
+            case .unselectFail:
+                return Color.red
+            case .normal:
+            return Color.black
+        }
+    }
 }
