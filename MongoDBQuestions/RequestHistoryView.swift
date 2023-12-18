b4888f892ce4a6a533d0c17f4c9bb6b29847c707
Stephen Hu <stephenhu@seismic.com>
2023-12-18 11:04:05 +0800

    adjust view

--- MongoDBQuestions/RequestHistoryView.swift
+++ MongoDBQuestions/RequestHistoryView.swift
@@ -8,27 +8,40 @@
 import SwiftUI
 
 struct RequestHistoryView: View {
+    let score:Int
     let questionHistoryInfos:[QuestionHistoryInfo]
     var body: some View {
         ScrollView{
-            ForEach(questionHistoryInfos, id:\.self.currentIndex)
+            
+            VStack
             {
-                questionHistoryInfo in
-                VStack
+                VStack(alignment: .leading)
                 {
-                    QuestionHistoryComponent(questionHistoryInfo: questionHistoryInfo)
+                    HStack{
+                        Text("Score:").font(.largeTitle)
+                        Text(String(score)).font(.largeTitle)
+                    }
                     
                     Divider()
+                }.frame(maxWidth: .infinity, alignment: .leading)
+                
+                ForEach(questionHistoryInfos, id:\.self.currentIndex)
+                {
+                    questionHistoryInfo in
+                    VStack
+                    {
+                        QuestionHistoryComponent(questionHistoryInfo: questionHistoryInfo)
+                        
+                        Divider()
+                    }
                 }
-
             }
-            
         }
     }
 }
 
 #Preview {
-    RequestHistoryView(questionHistoryInfos: [
+    RequestHistoryView(score: 100, questionHistoryInfos: [
         QuestionHistoryInfo ( currentIndex: 2, total: 15, questionDescrption: "Amongst which of the following is / are true about MongoDB?", selectOptionInfos: [SelectOptionInfo(optionId: "1", optionValue: "test value", questionId: "1", selectState: .selectRight),SelectOptionInfo(optionId: "2", optionValue: "test value 2", questionId: "2", selectState: .selectFail), SelectOptionInfo(optionId: "4", optionValue: "test value 4", questionId: "1", selectState: .unselectFail), SelectOptionInfo(optionId: "3", optionValue: "test value 3", questionId: "1", selectState: .normal) ]),
         QuestionHistoryInfo ( currentIndex: 3, total: 15, questionDescrption: "Amongst which of the following is / are true about MongoDB?", selectOptionInfos: [SelectOptionInfo(optionId: "1", optionValue: "test value", questionId: "1", selectState: .selectRight),SelectOptionInfo(optionId: "2", optionValue: "test value 2", questionId: "2", selectState: .selectFail), SelectOptionInfo(optionId: "4", optionValue: "test value 4", questionId: "1", selectState: .unselectFail), SelectOptionInfo(optionId: "3", optionValue: "test value 3", questionId: "1", selectState: .normal) ])
     ])
