77bfa71cdbdc9c135bcf81f642b2038041bb417a
Stephen Hu <stephenhu@seismic.com>
2024-01-17 11:31:47 +0800

    enrich QuestionDetailContext

--- MongoDBQuestions/Models/QuestionDetailContext.swift
+++ MongoDBQuestions/Models/QuestionDetailContext.swift
@@ -8,7 +8,19 @@
 import Foundation
 class QuestionDetailContext:ObservableObject
 {
-    @Published var currentIndex:Int
+    @Published var currentIndex:Int = 0
+    
+    @Published var currentQuestionDesription:String = ""
+    
+    @Published var currentQuestionOptions:[QuestionOption] = []
+    
+    let totalCount:Int = 10
+    
+    let questions:[Question] = []
+    
+    var selectOptionIds:[String] = []
+    
+    var questionHistoryInfos: [QuestionHistoryInfo] = []
     
     init(currentIndex: Int) {
         self.currentIndex = currentIndex
