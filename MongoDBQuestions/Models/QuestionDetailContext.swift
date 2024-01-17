766f7f250ca204cd2d93a660418cc86cf401cb2d
Stephen Hu <stephenhu@seismic.com>
2024-01-17 12:45:04 +0800

    imrpve question detail context

--- MongoDBQuestions/Models/QuestionDetailContext.swift
+++ MongoDBQuestions/Models/QuestionDetailContext.swift
@@ -13,6 +13,10 @@ class QuestionDetailContext:ObservableObject
     @Published var currentQuestionDesription:String = ""
     
     @Published var currentQuestionOptions:[QuestionOption] = []
+
+    var currentQuestion:Question {
+        return questions[currentIndex]
+    }
     
     let totalCount:Int = 10
     
@@ -21,8 +25,33 @@ class QuestionDetailContext:ObservableObject
     var selectOptionIds:[String] = []
     
     var questionHistoryInfos: [QuestionHistoryInfo] = []
+
+    var hasNext:Bool {
+        return currentIndex  < totalCount
+    }
     
     init(currentIndex: Int) {
         self.currentIndex = currentIndex
     }   
+
+    func completeCurrent()
+    {
+        let selectOptions = currentQuestionOptions.filter { (option) -> Bool in
+            selectOptionIds.contains(option.optionId)
+        }
+
+        let questionHistoryInfo = QuestionHistoryInfo.FromQuestion(question: currentQuestion, currentIndex: currentIndex, total: totalCount, selectOptions: selectOptions)
+        questionHistoryInfos.append(questionHistoryInfo)
+    }
+
+    func next()
+    {
+        if hasNext
+        {
+            currentIndex += 1
+            currentQuestionDesription = currentQuestion.questionDescription
+            currentQuestionOptions = currentQuestion.options
+            selectOptionIds = []
+        }
+    }
 }
